5bc194d3a6cd7bff51d194503a295c7266cb8023
emy <106605303+EmilliMachado@users.noreply.github.com>
2024-09-20 11:11:39 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,12 @@
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente`
+}
+
+vizualizarInformacoesGlobais()
+
