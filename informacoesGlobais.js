286a5aefd773370da180ea46a8441a1d581b21cc
emy <106605303+EmilliMachado@users.noreply.github.com>
2024-10-04 08:22:25 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,12 +1,16 @@
-const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
+const url = 'htpps://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function vizualizarInformacoesGlobais() {
+async function vizualizarinformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    console.log(paragrafo)
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
+vizualizarinformacoesGlobais()
 
